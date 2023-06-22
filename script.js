c5e3bf47563c2a10872d52e9cb5dd6d4b8c2e167
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-06-22 23:43:38 +0300

    js add

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function checkSSN() {
-   const ssn = document.getElementById("ssn").value;
-    const ssnRegex = /^\d{3}-\d{2}-\d{4}$/;
+// function checkSSN() {
+//    const ssn = document.getElementById("ssn").value;
+//     const ssnRegex = /^\d{3}-\d{2}-\d{4}$/;
 
-    if (ssnRegex.test(ssn)) {
+//     if (ssnRegex.test(ssn)) {
+//         document.getElementById("result").innerHTML = "SSN geçerlidir.";
+//     } else {
+//         document.getElementById("result").innerHTML = "Geçerli bir SSN giriniz (örn: 123-45-6789).";
+//     }
+// }
+function checkSSN() {
+    var ssn = document.getElementById("ssn").value;
+    var ssnWithoutDashes = ssn.replace(/-/g, "");
+
+    if (ssn.length === 11 && !isNaN(ssnWithoutDashes)) {
         document.getElementById("result").innerHTML = "SSN geçerlidir.";
     } else {
         document.getElementById("result").innerHTML = "Geçerli bir SSN giriniz (örn: 123-45-6789).";
